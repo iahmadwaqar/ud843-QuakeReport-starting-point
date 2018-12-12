7e5712374a05a263008081873aec2a1cb4363e19
Waqar Ahmad <ahmadwaqar@outlook.com>
2018-12-13 02:16:44 +0500

    Checking Commit

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -47,6 +47,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
+        fun();
     }
 
     private void fun(){
